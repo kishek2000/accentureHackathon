9120184472ff2b276136978aef098fa30a505bd0
root <root@vultr.guest>
2021-01-16 07:16:28 +0000

    Added ed configurable timespan to the rendered line graph + seed data for every category

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,54 +1,88 @@
 import React from 'react';
 import { Line } from '@reactchartjs/react-chart.js'
+import { topicAnalytics } from '../store/data' 
 
-const data = {
-  labels: [
-    '10/1/21', '11/1/21', '12/1/21', '13/1/21', '14/1/21', '15/1/21', '16/1/21'
-  ],
-  datasets: [
-    {
-      label: '___CATEGORY_HERE___ Proficiency this __TIME_RANGE__',
-      data: [12, 19, 3, 5, 2, 3],
-      fill: false,
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgba(255, 99, 132, 0.2)',
+const uppercaseAll = (text) => {
+  if (text) {
+    return text.toUpperCase();
+  }
+}
+
+const LineChart = ({ category, timeResolution }) => {
+  // TODO: Replace hardcoded dates 
+  let dates = [];
+  switch (timeResolution) {
+    case "week":
+      dates = ['10/1/21', '11/1/21', '12/1/21', '13/1/21', '14/1/21', '15/1/21', '16/1/21'];
+      break;
+    case "month":
+      dates = [
+        '20/12/20', '21/12/20', '22/12/20', '23/12/20', '24/12/20', '25/12/20', '26/12/20',
+        '27/12/20', '28/12/20', '29/12/20', '30/12/20', '31/12/20', '1/1/21', '2/1/21',
+        '3/1/21', '4/1/21', '5/1/21', '6/1/21', '7/1/21', '8/1/21', '9/1/21',
+        '10/1/21', '11/1/21', '12/1/21', '13/1/21', '14/1/21', '15/1/21', '16/1/21'];
+      break;
+    default:
+      dates = ['10/1/21', '11/1/21', '12/1/21', '13/1/21', '14/1/21', '15/1/21', '16/1/21'];
+      break;
+  }
+  const data = {
+    labels: dates,
+    datasets: topicAnalytics.map((eachTopic) => {
+      let datapoints = eachTopic.data;
+      switch(timeResolution) {
+        case "week":
+          datapoints = datapoints.slice(23, 30);
+          break;
+        case "month":
+          datapoints = datapoints.slice(0, 30);
+          break;
+        default:
+          break;
+      }
+      return {
+        label: `Proficiency in ${uppercaseAll(eachTopic.label)} this ${uppercaseAll(timeResolution)}`,
+        data: datapoints,
+        fill: false,
+        backgroundColor: eachTopic.colour,
+        borderColor: eachTopic.colour,
+      };
+    }),
+  };
+  const options = {
+    legend: {
+      labels: {
+        fontColor: "black",
+      }
     },
-  ],
-}
-
-const options = {
-  scales: {
-    yAxes: [
-      {
-        ticks: {
-          beginAtZero: true,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Proficiency (%)'
+          }
         },
-        scaleLabel: {
-          display: true,
-          labelString: 'Proficiency (%)'
-        }
-      },
-    ],
-    xAxes: [
-      {
-        scaleLabel: {
-          display: true,
-          labelString: 'Time (days)'
-        }
-      },
-    ],
-  },
-}
-
-const LineChart = () => (
-  <div style={{padding: "50px"}}>
-    <div className='header'>
-      <h1 
-        style={{textAlign: "center"}} 
-        className='title'>___'s progress</h1>
+      ],
+      xAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: 'Date'
+          }
+        },
+      ],
+    },
+  };
+  
+  return (
+    <div style={{padding: "50px"}}>
+      <Line data={data} options={options} />
     </div>
-    <Line data={data} options={options} />
-  </div>
-)
+  );
+}
 
 export default LineChart
