0cf5ceaf734167310daf7f1bc41c73e0ea8d32b4
Lachlan Harvey <lachlan.harvey@student.unsw.edu.au>
2021-01-16 17:38:25 +1100

    added

---
+++ src/store/courses.js
@@ -0,0 +1,23 @@
+
+export const courses = {"courses": [
+            { 
+                "title": "Shapes", 
+                "thumbnail": "shapesThumbnail.png", 
+                "id": "shapes01" 
+            }, 
+            { 
+                "title": "Colours", 
+                "thumbnail": "coloursThumbnail.png",
+                "id": "colours01"
+            },
+            { 
+                "title": "Verbs", 
+                "thumbnail": "verbsThumbnail.png",
+                "id": "verbs01"
+            },
+            { 
+                "title": "Nouns", 
+                "thumbnail": "nounsThumbnail.png",
+                "id": "nouns01"
+            }
+        ]}
