e23ccd15579bac0fb35ec2c169d87cb5a83725c4
Lachlan Harvey <lachlan.harvey17@gmail.com>
2021-02-28 13:48:02 +1100

    Added secret env to gitignore

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,5 @@
 export GALACTIC_ED_DEV_MODE=true
 export FLASK_APP=galactic-ed
+# Enables Flask's debug features such as live reloading when starting the server with Flask run
 export FLASK_ENV=development
 flask run
