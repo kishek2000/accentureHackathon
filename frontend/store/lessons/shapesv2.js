ab61a9164181475f2f82db7c5ff2030ebe6057b1
Liam <61527066+bubble-hiss@users.noreply.github.com>
2021-03-03 13:39:34 +1100

    Redid shapes with new JSON schema

---
+++ frontend/store/lessons/shapesv2.js
@@ -0,0 +1,161 @@
+export const shapesLessonOne = {
+    "_id": "shapes_lesson_1",
+    "course": "shapes",
+    "lesson": "What's that Shape?",
+    "prompt": "Tap on the shape to discover what it is!",
+    "questions": [
+      {
+        "shapes": [{ "shape": "square", "colour": 0 }],
+        "correctShape": "square",
+        "difficulty": 1,
+        "averageTime": 3
+      },
+      {
+        "shapes": [{ "shape": "circle", "colour": 0 }],
+        "correctShape": "circle",
+        "difficulty": 1,
+        "averageTime": 1,
+      },
+      {
+        "shapes": [{ "shape": "square", "colour": 0 }],
+        "correctShape": "square",
+        "difficulty": 1,
+        "averageTime": 3
+      },
+    ],
+  };
+
+  export const shapesLessonTwo = {
+    "_id": "shapes_lesson_2",
+    "course": "shapes",
+    "lesson": "What's that Shape?",
+    "prompt": "Match the right shape from the prompt.",
+    "questions": [
+      {
+        "shapes": [{ "shape": "square", "colour": 0 }],
+        "correctShape": "square",
+        "difficulty": 1,
+        "averageTime": 3
+      },
+      {
+        "shapes": [
+          { "shape": "square", "colour": 0 },
+          { "shape": "circle", "colour": 200 },
+        ],
+        "correctShape": "square",
+        "difficulty": 2,
+        "averageTime": 6
+      },
+      {
+        "shapes": [
+          { "shape": "square", "colour": 0 },
+          { "shape": "rectangle", "colour": 45 },
+          { "shape": "rectangle", "colour": 300 },
+        ],
+        "correctShape": "square",
+        "difficulty": 3,
+        "averageTime": 10
+      },
+    ],
+  };
+
+  export const shapesLessonThree = {
+    "_id": "shapes_lesson_3",
+    "course": "shapes",
+    "lesson": "What's that Shape?",
+    "prompt": "Match the right shape from the prompt.",
+    "questions": [
+      {
+        "shapes": [
+          { "shape": "square", "colour": 0 },
+          { "shape": "circle", "colour": 200 },
+        ],
+        "correctShape": "square",
+        "difficulty": 2,
+        "averageTime": 6
+      },
+      {
+        "shapes": [
+          { "shape": "circle", "colour": 200 },
+          { "shape": "rectange", "colour": 300 },
+        ],
+        "correctShape": "circle",
+        "difficulty": 2,
+        "averageTime": 6
+      },
+      {
+        "shapes": [
+          { "shape": "square", "colour": 0 },
+          { "shape": "rectangle", "colour": 300 },
+        ],
+        "correctShape": "rectangle",
+        "difficulty": 3,
+        "averageTime": 8
+      },
+      {
+        "shapes": [
+          { "shape": "square", "colour": 0 },
+          { "shape": "rectangle", "colour": 45 },
+          { "shape": "explosion", "colour": 100 },
+        ],
+        "correctShape": "rectangle",
+        "difficulty": 3,
+        "averageTime": 8
+      },
+    ],
+  };
+
+  export const shapesLessonFour = {
+    "_id": "shapes_lesson_4",
+    "course": "shapes",
+    "lesson": "What's that Shape?",
+    "prompt": "Match the right shape from the prompt.",
+    "questions": [
+      {
+        "shapes": [
+          { "shape": "square", "colour": 0 },
+          { "shape": "circle", "colour": 200 },
+          { "shape": "rectange", "colour": 200 },
+        ],
+        "correctShape": "square",
+        "difficulty": 3,
+        "averageTime": 9
+      },
+      {
+        "shapes": [
+          { "shape": "explosion", "colour": 200 },
+          { "shape": "rectangle", "colour": 300 },
+          { "shape": "triangle", "colour": 0 },
+        ],
+        "correctShape": "explosion",
+        "difficulty": 3,
+        "averageTime": 10,
+      },
+      {
+        "shapes": [
+          { "shape": "square", "colour": 0 },
+          { "shape": "circle", "colour": 50 },
+          { "shape": "explosion", "colour": 100 },
+          { "shape": "rectangle", "colour": 150 },
+          { "shape": "triangle", "colour": 200 },
+        ],
+        "correctShape": "triangle",
+        "difficulty": 4,
+        "averageTime": 14,
+      },
+      {
+        "shapes": [
+          { "shape": "square", "colour": 0 },
+          { "shape": "square", "colour": 300 },
+          { "shape": "explosion", "colour": 50 },
+          { "shape": "explosion", "colour": 250 },
+          { "shape": "rectangle", "colour": 100 },
+          { "shape": "rectangle", "colour": 200 },
+          { "shape": "circle", "colour": 150 },
+        ],
+        "correctShape": "circle",
+        "difficulty": 4,
+        "averageTime": 16,
+      },
+    ],
+  };
